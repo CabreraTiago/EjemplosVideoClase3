921d10596f6b3ea2340064b46df8131793bf3bdb
Tiago <tiagocabrera14@gmail.com>
2021-05-09 16:04:38 -0300

    globalContador con Locks

--- globalContador.py
+++ globalContador.py
@@ -2,17 +2,23 @@ import threading
 
 contador = 0
 
+lock = threading.Lock()
+
+
 def funcion():
+    lock.acquire()
     global contador
     for i in range(1000000):
         contador += 1
+    lock.release()
+
 
 print("Inicio programa principal")
 print("Valor Inicial: " + str(contador))
 
-thread_1=threading.Thread(target=funcion)
-thread_2=threading.Thread(target=funcion)
-thread_3=threading.Thread(target=funcion)
+thread_1 = threading.Thread(target=funcion)
+thread_2 = threading.Thread(target=funcion)
+thread_3 = threading.Thread(target=funcion)
 
 thread_1.start()
 thread_2.start()
@@ -23,7 +29,3 @@ thread_2.join()
 thread_3.join()
 
 print("Valor Final: " + str(contador))
-
-
-
-
